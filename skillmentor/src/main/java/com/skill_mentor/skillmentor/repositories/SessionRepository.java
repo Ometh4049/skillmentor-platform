711078eda7f31da356db480df199292eba657501
Ometh <poojanaometh7@gmail.com>
2026-03-12 21:08:05 +0530

    Remove skillmentor module and files
    Delete the entire skillmentor Maven module and related files

--- skillmentor/src/main/java/com/skill_mentor/skillmentor/repositories/SessionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.skill_mentor.skillmentor.repositories;
-
-import com.skill_mentor.skillmentor.entities.Session;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SessionRepository extends JpaRepository<Session,Long> {
-}
